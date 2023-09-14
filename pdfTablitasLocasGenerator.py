2ff735df70f176fd44244c66dd04dc9aac8135f9
voidexiled <l20070518@cdmadero.tecnm.mx>
2023-09-13 21:01:18 -0600

    vertical line

--- pdfTablitasLocasGenerator.py
+++ pdfTablitasLocasGenerator.py
@@ -6,7 +6,8 @@ import sys
 
 class PDFGenerator:
     charset = "utf-8"
-
+    width = 1.26
+    figureWidth, figureHeight = (width, width*1.5)
     def __init__(self, filename="tablitasLocas", size=(7.6, 12.4)):
         self.pdf = FPDF(orientation="P", unit="cm", format="A4")
         self.pdf.add_font(
@@ -21,16 +22,24 @@ class PDFGenerator:
         texts = [value.get("name") for key, value in self.data.items()]
         print(texts)
         self.pdf.add_page()
-
+        iterates = 0
         self.pdf.set_font("NunitoSansRegular", size=18)
         for text, figureSrc in self.data.items():
-            self.pdf.cell(0, 2, f"Doble comodin", ln=True, align="L")
+            if iterates == 7:
+                iterates = 0
+            self.pdf.cell(0, 2, "Doble comodin", ln=True, align="L")
             x, y = self.pdf.get_x(), self.pdf.get_y() - 2
+            
             # Añadir imagen
-            self.pdf.image(f"{figureSrc.get('src')}", x=8, y=y, w=1.26, h=1.26 * 1.5)
-            self.pdf.line(x, y + 3, x + 19, y + 3)
+            self.pdf.image(f"{figureSrc.get('src')}", x= 8 -self.figureWidth*1.25, y=y, w=1.26, h=1.26 * 1.5)
+            self.pdf.line(x-8, y + 3, x + 25, y + 3)
+            if iterates == 1:
+                self.pdf.line(x+7, y-6, x+7, y+30)
             self.pdf.set_xy(x, y + 4)
+            iterates += 1
 
+        
+            
         self.pdf.output(self.filename + ".pdf")
 
 
